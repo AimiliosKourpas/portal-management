ffb26d8f1aa80d08b4c1ce995e3dc93ce3a45b06
Aimilios Kourpas <aimiliosk2002@gmail.com>
2024-02-19 18:20:31 +0200

    - Generate Post and Category models. - Create associations between User, Post and Category models. - Create categories and posts database tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_18_091849) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_19_161106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.string "branch"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
