4f0a2661cb11e9007f81680aa3cebc80b4b97a97
Aimilios Kourpas <aimiliosk2002@gmail.com>
2024-02-19 19:46:58 +0200

    - Add faker gem - Inside the seeds.rb file create methods to generate   User, Category and Post records inside the development database

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    belongs_to :category
-  end
+  belongs_to :user
+  belongs_to :category
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_19_161106) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_19_174550) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_19_161106) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +44,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_19_161106) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
 end
