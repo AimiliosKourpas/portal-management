bf50e7c93566b28e7f266cca795c28bf36f29423
Aimilios Kourpas <aimiliosk2002@gmail.com>
2024-02-22 14:07:00 +0200

    Add specs for the update_pagination_partial_path helper method

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe PostsHelper, :type => :helper do
       expect(helper.no_posts_partial_path).to eq 'shared/empty_partial'
     end
   end
-  
+
   context '#post_format_partial_path' do
     it "returns a home_page partial's path" do
       allow(helper).to receive(:current_page?).and_return(true)
@@ -47,6 +47,24 @@ RSpec.describe PostsHelper, :type => :helper do
     it "returns a branch_page partial's path" do
       allow(helper).to receive(:current_page?).and_return(false)
       expect(helper.post_format_partial_path).to eq 'posts/post/branch_page'
+    end
+  end
+  
+  context '#update_pagination_partial_path' do
+    it "returns an update_pagination partial's path" do
+      posts = double('posts', :next_page => 2)
+      assign(:posts, posts)
+      expect(helper.update_pagination_partial_path).to(
+        eq 'posts/posts_pagination_page/update_pagination'
+      )
+    end
+
+    it "returns a remove_pagination partial's path" do
+      posts = double('posts', :next_page => nil)
+      assign(:posts, posts)
+      expect(helper.update_pagination_partial_path).to(
+        eq 'posts/posts_pagination_page/remove_pagination'
+      )
     end
   end
 end
