19cce0ee4b24ce26516901dd874e548e70e44b8e
Aimilios Kourpas <aimiliosk2002@gmail.com>
2024-02-29 05:41:17 +0200

    - Update the append_previous_messages_partial_path helper method in   Shared::MessagesHelper - Update the replace_link_to_private_messages_partial_path method in   Private::MessagesHelper

--- app/helpers/shared/messages_helper.rb
+++ app/helpers/shared/messages_helper.rb
@@ -1,8 +1,14 @@
 module Shared::MessagesHelper
   def append_previous_messages_partial_path
     'shared/load_more_messages/window/append_messages'
+    # if a conversation is opened in the messenger
+    if @is_messenger == 'true'
+      'shared/load_more_messages/messenger/append_messages'
+    else
+      'shared/load_more_messages/window/append_messages'
+    end
   end
-  
+
   # if there are no previous messages
   def remove_link_to_messages
     if @is_messenger == 'false'
